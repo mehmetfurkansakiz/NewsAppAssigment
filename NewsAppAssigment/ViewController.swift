63de9fe5d0ae0a52279668945242978a91765905
mehmetfurkansakiz <mehmetfurkansakiz@gmail.com>
2025-03-11 21:17:03 +0300

    Initial Commit

---
+++ NewsAppAssigment/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NewsAppAssigment
+//
+//  Created by furkan sakız on 11.03.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
